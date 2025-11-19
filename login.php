85a31c06be9ba703057109a16ba10aec7251bf2f
yolandast11 <yolandanst03@gmail.com>
2025-11-19 23:10:21 +0700

    Mendesain halaman login

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <h2>Form Login</h2>
+    <form method="post">
+        Username: <input type="text" name="username" required><br><br>
+        Password: <input type="password" name="password" required><br><br>
+        <button type="submit">Login</button>
+        <button type="reset">Batal</button>
+    </form>
+
+</body>
+</html>
